d301138a71fa77bcc4723576371d7d6b624120e9
Sahil Lodhi <sahillodhi369@gmail.com>
2026-01-19 11:03:54 +0530

    postorder traversal problem on leetcode

---
+++ PostorderTraversal/postOrder.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+
+void helper(TreeNode* root, vector<int> &ans){
+    if(root == NULL){
+        return;
+    }
+
+    helper(root->left, ans);
+    helper(root->right, ans);
+    ans.push_back(root->val);
+
+    return;
+}
+
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        helper(root, ans);
+
+        return ans;
+    }
+};
