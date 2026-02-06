e85106c9d294216b947857b47c3999391b5aa07e
Sahil Lodhi <sahillodhi369@gmail.com>
2026-02-06 23:24:12 +0530

    sorted LL to Tree problem on leetcode

---
+++ SortedLLToTree/code.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    TreeNode* helper(vector<int> &arr, int st, int end){
+        if(st > end){
+            return NULL;
+        }
+        int mid = st+(end-st)/2;
+        TreeNode* curr = new TreeNode(arr[mid]);
+
+        curr->left = helper(arr, st, mid-1);
+        curr->right = helper(arr, mid+1, end);
+
+        return curr;
+    }
+
+
+    TreeNode* sortedListToBST(ListNode* head) {
+        vector<int> arr;
+        ListNode* temp = head;
+        while(temp){
+            arr.push_back(temp->val);
+            temp = temp->next;
+        }
+        return helper(arr, 0, arr.size()-1);
+    }
+};
