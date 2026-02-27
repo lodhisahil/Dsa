6e03d84e9004f678e85e7b58c644029ed78f3804
Sahil Lodhi <sahillodhi369@gmail.com>
2026-02-27 14:26:33 +0530

    upper bound problem on GFG

---
+++ GeeksForGeeks/UpperBound/upperBound.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    int upperBound(vector<int>& arr, int target) {
+        // code here
+        int st = 0;
+        int end = arr.size()-1;
+        int ans = arr.size();
+        
+        while(st <= end){
+            int mid = st + (end-st)/2;
+            if(arr[mid] > target){
+                ans = mid;
+                end = mid-1;
+            } else {
+                st = mid+1;
+            }
+        }
+        return ans;
+    }
+};
