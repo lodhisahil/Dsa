13ea0873bbf887f5586d47719579642effdeacbf
Sahil Lodhi <sahillodhi369@gmail.com>
2026-03-14 11:01:47 +0530

    search in 2d matrix II problem on leetcode

---
+++ LeetCode/SearchIn2DMatrixII/searchIn2DMatrixII.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int row = m-1;
+        int col = 0;
+        while(row >= 0 && col < n){
+            if(target == matrix[row][col]){
+                return true;
+            }else if(target < matrix[row][col]){
+                row--;
+            }else {
+                col++;
+            }
+        }
+        return false;
+    }
+};
