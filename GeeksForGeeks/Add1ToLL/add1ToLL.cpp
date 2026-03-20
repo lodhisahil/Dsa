9e1ac31da9648e6739f1d20a4a6a49363562da5e
Sahil Lodhi <sahillodhi369@gmail.com>
2026-03-20 11:59:43 +0530

    add 1 to LL problem on GFG

---
+++ GeeksForGeeks/Add1ToLL/add1ToLL.cpp
@@ -0,0 +1,43 @@
+/*
+
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+class Solution {
+  public:
+  
+    int helper(Node* temp){
+        int carry = 1;
+        if(temp == NULL){
+            return 1;
+        }
+        carry = helper(temp->next);
+        temp->data = temp->data + carry;
+        if(temp->data < 10){
+            return 0;
+        }
+        temp->data = 0;
+        return 1;
+    }
+    
+    Node* addOne(Node* head) {
+        // Your Code here
+        int carry = helper(head);
+        if(carry == 1){
+            Node* newNode = new Node(1);
+            newNode->next = head;
+            return newNode;
+        }
+        return head;
+    }
+};
