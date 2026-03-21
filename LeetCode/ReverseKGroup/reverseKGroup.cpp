72e31791fc573cfb4089eceebe958fd47f5e2649
Sahil Lodhi <sahillodhi369@gmail.com>
2026-03-21 21:57:05 +0530

    reverse k group problem iterative approach

--- LeetCode/ReverseKGroup/reverseKGroup.cpp
+++ LeetCode/ReverseKGroup/reverseKGroup.cpp
@@ -25,3 +25,54 @@ public:
         return prevNode;
     }
 };
+
+
+class Solution {
+public:
+    ListNode* findKthNode(ListNode* temp, int k){
+        while(temp != NULL && k > 1){
+            k--;
+            temp = temp->next;
+        }
+        return temp;
+    }
+
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* next = NULL;
+
+        while(curr != NULL){
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode* temp = head;
+        ListNode* prevNode = NULL;
+        while(temp != NULL){
+            ListNode* kthNode = findKthNode(temp, k);
+            if(kthNode == NULL){
+                if(prevNode){
+                    prevNode->next = temp;
+                }
+                break;
+            }
+            ListNode* nextNode = kthNode->next;
+            kthNode->next = NULL;
+            reverseList(temp);
+            if(temp == head){
+                head = kthNode;
+            }else{
+                prevNode->next = kthNode;
+            }
+            prevNode = temp;
+            temp = nextNode;
+        }
+        return head;
+    }
+};
