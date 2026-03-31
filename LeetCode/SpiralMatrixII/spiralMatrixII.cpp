459450d4bcd45c7b06a1b625155ead3dfe8041ba
Sahil Lodhi <sahillodhi369@gmail.com>
2026-03-31 10:34:09 +0530

    spiral matrix II preoblem on leetcode

---
+++ LeetCode/SpiralMatrixII/spiralMatrixII.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        vector<vector<int>> matrix(n, vector<int>(n, 0));
+        int count = 1;
+        int left = 0, right = n-1;
+        int top = 0, bottom = n-1;
+        while(left <= right && top <= bottom){
+            //1.left to right
+            for(int i=left; i<=right; i++){
+                matrix[top][i] = count;
+                count++;
+            }
+            top++;
+
+            //2. top to bottom
+            for(int i=top; i<=bottom; i++){
+                matrix[i][right] = count;
+                count++;
+            }
+            right--;
+
+            //3. right to left
+            if(left <= right){
+                for(int i=right; i>=left; i--){
+                    matrix[bottom][i] = count;
+                    count++;
+                }
+                bottom--;
+            }
+
+            //4. bottom to top
+            if(top <= bottom){
+                for(int i=bottom; i>=top; i--){
+                    matrix[i][left] = count;
+                    count++;
+                }
+                left++;
+            } 
+        }
+        return matrix;
+    }
+};
