ae587b3b55b74cf3ff69d25c890588c40abd3348
Sahil Lodhi <sahillodhi369@gmail.com>
2026-04-08 20:43:45 +0530

    intersection of 2 arrays problem on leetcode

---
+++ LeetCode/IntersectionOf2Array/intersection.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> s1(nums1.begin(), nums1.end());
+        unordered_set<int> result;
+        for(int num : nums2){
+            if(s1.find(num) != s1.end()){
+                result.insert(num);
+            }
+        }
+        return vector<int>(result.begin(), result.end());
+    }
+};
