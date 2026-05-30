01b3259878d839368fb993dd82322d66a29ba73e
Sahil Lodhi <sahillodhi369@gmail.com>
2026-05-30 23:04:31 +0530

    fizzbuzz problem on leetcode

---
+++ LeetCode/FizzBuzz/fizzBuzz.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> ans;
+        int i = 1;
+        while(ans.size() < n){
+            if(i % 15 == 0){
+                ans.push_back("FizzBuzz");
+            }else if(i % 3 == 0){
+                ans.push_back("Fizz");
+            }else if(i % 5 == 0){
+                ans.push_back("Buzz");
+            }else{
+                ans.push_back(to_string(i));
+            }
+            i++;
+        }
+        return ans;
+    }
+};
