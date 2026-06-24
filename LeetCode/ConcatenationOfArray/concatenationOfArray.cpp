1eb1f6d5df8aba7c0b567934bdd6acef0afa7f00
Sahil Lodhi <sahillodhi369@gmail.com>
2026-06-24 09:11:57 +0530

    concatenation of array problem on leetcode

---
+++ LeetCode/ConcatenationOfArray/concatenationOfArray.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n = nums.size();
+        int m = 2*n;
+        vector<int> ans(m, 0);
+        for(int i=0; i<n; i++){
+            ans[i] = nums[i];
+            ans[n+i] = nums[i];
+        }
+        return ans;
+    }
+};
