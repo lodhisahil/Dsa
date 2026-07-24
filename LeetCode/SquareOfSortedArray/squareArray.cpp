3ed97699ad0676c4a2c6b67ceca48215bccfc0af
Sahil Lodhi <sahillodhi369@gmail.com>
2026-07-24 22:49:02 +0530

    square of sorted array problem on leetcode

---
+++ LeetCode/SquareOfSortedArray/squareArray.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        vector<int> ans(nums.size());
+        int i = 0;
+        int j = nums.size()-1;
+        int k = nums.size()-1;
+        while(i <= j){
+            if(nums[i]*nums[i] > nums[j]*nums[j]){
+                ans[k] = nums[i]*nums[i];
+                k--;
+                i++;
+            }else{
+                ans[k] = nums[j]*nums[j];
+                k--;
+                j--;
+            }
+        }
+        return ans;
+    }
+};
