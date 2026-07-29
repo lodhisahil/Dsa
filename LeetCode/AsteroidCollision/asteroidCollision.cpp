95435cd0c06fadd9aff775fb48b0998ab8943ab2
Sahil Lodhi <sahillodhi369@gmail.com>
2026-07-29 22:44:29 +0530

    asteroid collision second approach

--- LeetCode/AsteroidCollision/asteroidCollision.cpp
+++ LeetCode/AsteroidCollision/asteroidCollision.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& ast) {
@@ -23,5 +24,28 @@ public:
         }
         reverse(ans.begin(), ans.end());
         return ans;
+    }
+};
+*/
+//Here we can use a array also in place of a stack
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& ast) {
+        vector<int> s;
+        for(int val: ast){
+            if(val > 0){
+                s.push_back(val);
+            }else{
+                while(s.size() > 0 && s.back() > 0 && s.back() < abs(val)){
+                    s.pop_back();
+                }
+                if(s.size() == 0 || s.back() < 0){
+                    s.push_back(val);
+                }else if(s.back() == abs(val)){
+                    s.pop_back();
+                }
+            }
+        }
+        return s;
     }
 };
