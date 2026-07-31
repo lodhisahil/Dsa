5b8410f053cdb3bfb7215ef75bed653361845a4f
Sahil Lodhi <sahillodhi369@gmail.com>
2026-07-31 17:30:22 +0530

    Max Consecutive Ones III problem on leetcode

---
+++ LeetCode/MaxConsecutiveOneslll/maxConsecutiveOnes.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int l = 0, r = 0;
+        int zeros = 0;
+        int maxLength = 0;
+        int length = 0;
+        while(r < nums.size()){
+            if(nums[r] == 0){
+                zeros++;
+                while(zeros > k){
+                    if(nums[l] == 0){
+                        zeros--;
+                    }
+                    l++;
+                }
+            }
+            length = r - l + 1;
+            r++;
+            maxLength = max(maxLength, length);
+        }
+        return maxLength;
+    }
+};
