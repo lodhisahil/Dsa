d780cc57f722258b048ce0e5c5cf925c202e1ba7
Sahil Lodhi <sahillodhi369@gmail.com>
2026-08-09 11:59:27 +0530

    Subarray Sum Equals K better code

--- LeetCode/SubarraysSumIsTarget/countSubarrays.cpp
+++ LeetCode/SubarraysSumIsTarget/countSubarrays.cpp
@@ -26,3 +26,20 @@ public:
         return count;
     }
 };
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int sum = 0;
+        int count = 0;
+        unordered_map<int, int> m;
+        m[0] = 1;
+        for(int i=0; i<nums.size(); i++){
+            sum += nums[i];
+            int rem = sum - k;
+            count += m[rem];
+            m[sum]++;
+        }
+        return count;
+    }
+};
