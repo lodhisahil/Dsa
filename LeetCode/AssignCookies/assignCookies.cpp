ceffe0a55f9b04aa088964a6ab982aa90aa9b535
Sahil Lodhi <sahillodhi369@gmail.com>
2026-08-15 09:47:02 +0530

    assign cookies problem on leetcode

---
+++ LeetCode/AssignCookies/assignCookies.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        int i = g.size() - 1;
+        int j = s.size() - 1;
+        int count = 0;
+        while(i >= 0 && j >= 0){
+            if(g[i] <= s[j]){
+                count++;
+                j--;
+            }
+            i--;
+        }
+        return count;
+    }
+};
