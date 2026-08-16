450c151d0f9dc7c1df930e4bcf03b091ac63699c
Sahil Lodhi <sahillodhi369@gmail.com>
2026-08-16 12:29:06 +0530

    jump game problem on leetcode

---
+++ LeetCode/JumpGame/jumpGame.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxIndex = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(i > maxIndex){
+                return false;
+            }
+            if(maxIndex >= nums.size()){
+                return true;
+            }
+            maxIndex = max(maxIndex, i + nums[i]);
+        }
+        return true;
+    }
+};
