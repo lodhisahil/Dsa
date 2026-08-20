e1cbaec0cbbea9d429f69e9b614eb9df9a681f0f
Sahil Lodhi <sahillodhi369@gmail.com>
2026-08-20 14:47:55 +0530

    factorial N problem on GFG

---
+++ GeeksForGeeks/FactorialN/factorialN.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+  
+    int fact(int n){
+        if(n == 0){
+            return 1;
+        }
+        return n * fact(n-1);
+    }
+  
+    int factorial(int n) {
+        // code here
+        return fact(n);
+    }
+};
