25d1dd039ffa78692490ceb6b275beda05a53b19
Sahil Lodhi <sahillodhi369@gmail.com>
2026-08-30 11:03:53 +0530

    N-Qweens problem column approach on leetcode

--- LeetCode/N-Queens/nQueens.cpp
+++ LeetCode/N-Queens/nQueens.cpp
@@ -1,3 +1,5 @@
+// Row Approach
+
 class Solution {
 public:
 
@@ -51,6 +53,59 @@ public:
         vector<string> board(n, string(n, '.'));
         vector<vector<string>> ans;
         nQueens(board, 0, n, ans);
+        return ans;
+    }
+};
+
+
+// Column Approach
+
+class Solution {
+public:
+    bool isSafe(vector<string> &board, int row, int col, int n){
+        //horizontal
+        for(int j=0; j<n; j++){
+            if(board[row][j] == 'Q'){
+                return false;
+            }
+        }
+        //vertical
+        for(int i=0; i<n; i++){
+            if(board[i][col] == 'Q'){
+                return false;
+            }
+        }
+        //left lower diagonal
+        for(int i=row, j=col; i<n && j>=0; i++,j--){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
+        //left upper diagonal
+        for(int i=row, j=col; i>=0 && j>=0; i--,j--){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
+        return true;
+    }
+    void helper(vector<string>& board, vector<vector<string>>& ans, int n, int column){
+        if(column == n){
+            ans.push_back(board);
+            return;
+        }
+        for(int i = 0; i < n; i++){
+            if(isSafe(board, i, column, n)){
+                board[i][column] = 'Q';
+                helper(board, ans, n, column + 1);
+                board[i][column] = '.';
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string> board(n, string(n, '.'));
+        vector<vector<string>> ans;
+        helper(board, ans, n, 0);
         return ans;
     }
 };
