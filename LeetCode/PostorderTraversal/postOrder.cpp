c907329ad38f4e6290791039558807d74eed07d7
Sahil Lodhi <sahillodhi369@gmail.com>
2026-09-02 11:06:31 +0530

    postorder traversal Iterative approach one stack

--- LeetCode/PostorderTraversal/postOrder.cpp
+++ LeetCode/PostorderTraversal/postOrder.cpp
@@ -49,3 +49,34 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        TreeNode* curr = root;
+        TreeNode* temp;
+        stack<TreeNode*> st;
+        while(curr != NULL || !st.empty()){
+            if(curr != NULL){
+                st.push(curr);
+                curr = curr->left;
+            }else{
+                temp = st.top()->right;
+                if(temp == NULL){
+                    temp = st.top();
+                    st.pop();
+                    ans.push_back(temp->val);
+                    while(!st.empty() && temp == st.top()->right){
+                        temp = st.top();
+                        st.pop();
+                        ans.push_back(temp->val);
+                    }
+                }else{
+                    curr = temp;
+                }
+            }
+        }
+        return ans;
+    }
+};
