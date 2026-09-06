714bae49a276aec8cb76991092b9780e23db1179
Sahil Lodhi <sahillodhi369@gmail.com>
2026-09-06 22:14:33 +0530

    balanced binary tree problem optimal approach

--- LeetCode/BalancedBinaryTree/balancedBinaryTree.cpp
+++ LeetCode/BalancedBinaryTree/balancedBinaryTree.cpp
@@ -32,3 +32,22 @@ public:
         return false;
     }
 };
+
+// OPTIMAL APPROACH 
+class Solution {
+public:
+    int height(TreeNode* root){
+        if(root == NULL){
+            return 0;
+        }
+        int lh = height(root->left);
+        int rh = height(root->right);
+        if(lh == -1 || rh == -1 || abs(lh-rh) > 1){
+            return -1;
+        }
+        return 1 + max(lh, rh);
+    }
+    bool isBalanced(TreeNode* root) {    
+        return height(root) == -1 ? false : true;
+    }
+};
