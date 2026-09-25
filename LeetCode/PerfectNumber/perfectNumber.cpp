ae8e06f2c27b8dc6be46dad586635f620bcf0a87
Sahil Lodhi <sahillodhi369@gmail.com>
2026-09-25 10:51:44 +0530

    perfect number problem on leetcode

---
+++ LeetCode/PerfectNumber/perfectNumber.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int sum = 1;
+        for(int i=2; i <= num/2; i++){
+            if(num % i == 0){
+                sum += i;
+            }
+        }
+        return sum == num;
+    }
+};
